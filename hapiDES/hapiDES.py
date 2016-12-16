bd6a28b4b23734b9830f5f487cf43b3ac137e310
Vinayaka Jyothi <vj338@nyu.edu>
2016-12-15 22:49:33 -0500

    sd

--- hapiDES/hapiDES.py
+++ hapiDES/hapiDES.py
@@ -3,7 +3,7 @@ from pynq import MMIO
 from pynq import Overlay
 from hapiDES import general_const
 
-hapiDES_overlay = None
+mmio = MMIO(0x43C40000,0x00010000)
 
 class hapiDES():
     """Class to control the custom IP hardware (HAPI)
@@ -20,7 +20,7 @@ class hapiDES():
        
    """
 
-    mmio = MMIO(0x43C40000,0x00010000)
+    
     def __init__(self):
         self.bitfile = general_const.BITFILE
         self.overlay = Overlay(self.bitfile)
@@ -37,55 +37,3 @@ class hapiDES():
         print("DES ACCELERATOR RESET")
         config_reg=0x80000001
         self.mmio.write(0,config_reg)
-
-    def des_status(self):
-        config_reg=mmio.read(0)
-        print("DES ACCEL STATUS")
-        if config_reg==0x80000001:
-            print("RESET")
-            print("MODE:ENCRYPT")
-        elif config_reg==0x80000000:
-            print("RESET")
-            print("MODE:DECRYPT")
-        elif config_reg==0x00000000:
-            print("ACTIVE (NOT IN RESET)")
-            print("MODE:DECRYPT")
-        elif config_reg==0x00000001:
-            print("ACTIVE (NOT IN RESET)")
-            print("MODE:ENCRYPT")
-        else:
-            print("ERROR: NOT CONFIGURED PROPERLY --> SOMETHING IS WRONG")
-    
-    def set_oper_encrypt(self):
-        config_reg=0x00000001
-        mmio.write(0,config_reg)
-        print("DES ACCELERATOR IS SET TO ENCRYPT")
-        print("-->SET A NEW KEY OR WILL USE THE PREVIOUS SET KEY")
-        
-    def set_oper_decrypt(self):
-        config_reg=0x00000000
-        mmio.write(0,config_reg)
-        print("DES ACCELERATOR IS SET TO DECRYPT")
-        print("-->SET A NEW KEY OR WILL USE THE PREVIOUS SET KEY")
-
-    def result(self):     
-        result_ENC_DEC[0]= hex(mmio.read(20))
-        result_ENC_DEC[1]= hex(mmio.read(24))
-        return result_ENC_DEC
-    
-    def set_key(self,data):
-        mmio.write(12,data[0])
-        mmio.write(16,data[1])
-        print("CRYPTO KEY UPDATED")
- 
-    def encrypt(self,data):    
-        mmio.write(4,data[0])
-        mmio.write(8,data[1])
-        self.result()
-        return result_ENC_DEC
-    
-    def decrypt(self,data):
-        mmio.write(4,data[0])
-        mmio.write(8,data[1])
-        self.result()
-        return result_ENC_DEC
