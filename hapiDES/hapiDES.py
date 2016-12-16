9987d90f9e196704cd18480c176e808124666f38
Vinayaka Jyothi <vj338@nyu.edu>
2016-12-15 23:16:00 -0500

    asd

--- hapiDES/hapiDES.py
+++ hapiDES/hapiDES.py
@@ -18,17 +18,38 @@ class hapiDES():
    """
 
     
+from pynq import PL
+from pynq import MMIO
+from pynq import Overlay
+from hapiDES import general_const
+DES_overlay = None
+
+class hapiDES():
+    """Class to control the custom IP hardware (HAPI)
+
+    Attributes
+    ----------
+    bitfile : str
+    Absolute path to bitstream
+    
+    data : List of size2 (int or hex values) 
+       Input Data for Encryption or Decryption: data[0] is MSB 32 bits and data [1] is LSB 32 bits
+    result_ENC_DEC : List of size2 (hex value) 
+       Return results of Encryption or Decryption: result_ENC_DEC[0] is MSB 32 bits and result_ENC_DEC[1] is LSB 32 bits
+       
+   """
+
+    
     def __init__(self):
         self.bitfile = general_const.BITFILE
-        self.overlay = Overlay(self.bitfile)
-        mmio = MMIO(0x43C40000,0x00010000)
-        print(mmio)
-    #global mmio = MMIO(0x43C40000,0x00010000)
-        global result_ENC_DEC
-        result_ENC_DEC=[0,0]
-        if not Overlay.is_loaded(self.overlay):
-            self.overlay.download()
+        if PL.bitfile_name != self.bitfile:
+                self.download_bitstream()
 
+    def download_bitstream(self):
+        global DES_overlay
+        DES_overlay = Overlay(self.bitfile)
+        DES_overlay.download()
+        DES_overlay.ip_dict
     def reset_des_accel(self):
         config_reg=0x80000001
         #mmio.write(0,config_reg)
