db054c9ef8d6a1cabd13dba058c925f35c86c4cf
Vinayaka Jyothi <vj338@nyu.edu>
2016-12-15 23:22:51 -0500

    Complete Script with functions

--- hapiDES/hapiDES.py
+++ hapiDES/hapiDES.py
@@ -58,3 +58,62 @@ class hapiDES():
         print("DES ACCELERATOR RESET")
         config_reg=0x80000001
         self.mmio.write(0,config_reg)
+
+    def des_status(self):
+        global mmio
+        config_reg=mmio.read(0)
+        print("DES ACCEL STATUS")
+        if config_reg==0x80000001:
+            print("RESET")
+            print("MODE:ENCRYPT")
+        elif config_reg==0x80000000:
+            print("RESET")
+            print("MODE:DECRYPT")
+        elif config_reg==0x00000000:
+            print("ACTIVE (NOT IN RESET)")
+            print("MODE:DECRYPT")
+        elif config_reg==0x00000001:
+            print("ACTIVE (NOT IN RESET)")
+            print("MODE:ENCRYPT")
+        else:
+            print("ERROR: NOT CONFIGURED PROPERLY --> SOMETHING IS WRONG")
+    
+    def set_oper_encrypt(self):
+        global mmio
+        config_reg=0x00000001
+        mmio.write(0,config_reg)
+        print("DES ACCELERATOR IS SET TO ENCRYPT")
+        print("-->SET A NEW KEY OR WILL USE THE PREVIOUS SET KEY")
+        
+    def set_oper_decrypt(self):
+        global mmio
+        config_reg=0x00000000
+        mmio.write(0,config_reg)
+        print("DES ACCELERATOR IS SET TO DECRYPT")
+        print("-->SET A NEW KEY OR WILL USE THE PREVIOUS SET KEY")
+
+    def result(self):     
+        global mmio
+        result_ENC_DEC[0]= hex(mmio.read(20))
+        result_ENC_DEC[1]= hex(mmio.read(24))
+        return result_ENC_DEC
+    
+    def set_key(self,data):
+        global mmio
+        mmio.write(12,data[0])
+        mmio.write(16,data[1])
+        print("CRYPTO KEY UPDATED")
+ 
+    def encrypt(self,data):  
+        global mmio
+        mmio.write(4,data[0])
+        mmio.write(8,data[1])
+        self.result()
+        return result_ENC_DEC
+    
+    def decrypt(self,data):
+        global mmio
+        mmio.write(4,data[0])
+        mmio.write(8,data[1])
+        self.result()
+        return result_ENC_DEC
